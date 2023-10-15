263a904d794a3d2b126228699e7a7f06130fdf6c
chaemina <103165845+chaemina@users.noreply.github.com>
2023-10-15 23:34:03 +0900

    Revert "전남대_18조_Garden_6주차(dev->master)"

--- src/components/account/constants/SIGNUP.js
+++ src/components/account/constants/SIGNUP.js
@@ -1,61 +1,31 @@
 const SIGNUP = Object.freeze([
   {
-    name: "firstName",
+    id: "FirstName",
     label: "First Name",
     type: "text",
-    placeholder: "first Name",
-    variant: "filled",
-    rules: {
-      required: "The input box must not be empty.",
-      pattern: {
-        value: /^(?![!@#$%^&*()\-_+=]).*$/,
-        message: "Names cannot contain special characters and numbers.",
-      },
-    },
+    placeholder: "First Name",
+    className: "w-50%",
   },
   {
-    name: "lastName",
+    id: "LastName",
     label: "Last Name",
     type: "text",
     placeholder: "Last Name",
-    variant: "filled",
-    rules: {
-      required: "The input box must not be empty.",
-      pattern: {
-        value: /^(?![!@#$%^&*()\-_+=]).*$/,
-        message: "Names cannot contain special characters and numbers.",
-      },
-    },
+    className: "w-50%",
   },
   {
-    name: "email",
+    id: "email",
     label: "Email",
     type: "email",
-    placeholder: "email address",
-    variant: "filled",
-    rules: {
-      required: "The input box must not be empty.",
-      pattern: {
-        value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-        message: "You must keep the format of the e-mail.",
-      },
-    },
+    placeholder: "Email Address",
+    className: "w-full",
   },
   {
-    name: "password",
+    id: "password",
     label: "Password",
     type: "password",
-    placeholder: "password",
-    variant: "filled",
-    rules: {
-      required: "The input box must not be empty.",
-      pattern: {
-        value:
-          /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()\-_+=])[a-zA-Z0-9!@#$%^&*()\-_+=]{8,20}$/,
-        message:
-          "Password must be within 8-20, including all English case, numbers, and special characters.",
-      },
-    },
+    placeholder: "Password",
+    className: "w-full",
   },
 ]);
 
--- src/layouts/GNB.jsx
+++ src/layouts/GNB.jsx
@@ -1,51 +1,115 @@
-import { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-import { navStructure } from "./navStructure";
+import { useNavState } from "/src/hooks/useNavState.js";
+
+const nav = [
+  {
+    mainNav: "Watching",
+    mainUrl: "watching",
+    sub: [
+      { subNav: "Video", url: ["watching/videos", "watching/video"] },
+      { subNav: "History", url: ["watching/History"] },
+    ],
+  },
+  {
+    mainNav: "Mentoring",
+    mainUrl: "mentoring",
+    sub: [
+      {
+        subNav: "List",
+        url: [
+          "mentoring/posts",
+          "mentoring/post",
+          "mentoring/write",
+          "mentoring/edit",
+        ],
+      },
+      { subNav: "Dashboard", url: ["mentoring/dashboard"] },
+    ],
+  },
+  {
+    mainNav: "Chatting",
+    mainUrl: "chatting",
+    sub: [
+      {
+        subNav: "Open Chatting",
+        url: [
+          "chatting/rooms",
+          "chatting/roomprofile",
+          "chatting/room",
+          "chatting/create",
+        ],
+      },
+    ],
+  },
+  {
+    mainNav: "My Page",
+    mainUrl: "mypage",
+    sub: [
+      { subNav: "Profile", url: ["mypage/profile", "mypage/profile/fix"] },
+      {
+        subNav: "Information",
+        url: ["mypage/information", "mypage/information/fix"],
+      },
+    ],
+  },
+];
 
 export default function GNB() {
-  const currentUrl = useLocation()
-    .pathname.replace(/\d/, "")
-    .replace(/^\/+|\/+$/g, "");
-
+  const { currentUrl, selectedmainNav, setselectedMainNav } = useNavState();
   // 로그인 기능 생기기 전까지 임시 로그인 판단 방식
   const [auth, setAuth] = useState(
     window.localStorage.getItem("token") ? true : false
   );
-
   // api 기능 생기기 전까지 임시 프로필 이미지 경로
   const profileImageUrl =
     "https://mblogthumb-phinf.pstatic.net/MjAyMDExMDFfMjIg/MDAxNjA0MjI4ODc1MDkx.itxFQbHQ_zAuNQJU7PCOlF0mmstYn2v4ZF4WygunqGIg.3jloNowx-eWU-ztCLACtYubVbATNdCFQLjgvYsynV1og.JPEG.gambasg/유튜브_기본프로필_주황.jpg?type=w400";
 
-  const handleLogOutClick = () => {
-    window.localStorage.removeItem("token");
-    setAuth(!!window.localStorage.getItem("token"));
+  useEffect(() => {
+    setselectedMainNav(
+      nav.find((val) => val.mainUrl === currentUrl.split("/")[0]).mainNav
+    );
+  }, [setselectedMainNav, currentUrl]);
+
+  const handleNavMouseLeave = () => {
+    setselectedMainNav(
+      nav.find((val) => val.mainUrl === currentUrl.split("/")[0]).mainNav
+    );
   };
 
-  const handleTestClick = () => {
-    if (auth) window.localStorage.removeItem("token");
-    else window.localStorage.setItem("token", "Bearer 1234");
-    setAuth(!!window.localStorage.getItem("token"));
+  const handleMainNavClick = (e) => {
+    setselectedMainNav(e.target.innerText);
+  };
+
+  const handleLogOutClick = () => {
+    window.localStorage.removeItem("token");
+    setAuth(window.localStorage.getItem("token") ? true : false);
   };
 
   return (
-    <nav className="fixed top-0 w-full h-20 bg-white text-green-900">
+    <nav
+      className="fixed top-0 w-full h-20 bg-white text-green-900"
+      onMouseLeave={handleNavMouseLeave}
+    >
       {/* 상단GNB */}
       <div className="h-12 px-16 border flex items-center">
         {/* 상단GNB - 상단Nav */}
         <div className="flex-1 flex justify-start space-x-4">
-          {navStructure.map((val) => (
-            <Link
-              key={val.mainNav}
+          {nav.map((val) => (
+            <button
+              key={`mainNav-${val.mainNav}`}
               className={`w-20 h-7 text-center text-sm${
+                val.mainNav === selectedmainNav ? " font-bold" : ""
+              }${
                 currentUrl.includes(val.mainUrl)
-                  ? " border-b-2 border-orange font-bold"
+                  ? " border-b-2 border-orange"
                   : ""
               }`}
-              to={val.sub[0].url[0]}
+              onClick={handleMainNavClick}
             >
               {val.mainNav}
-            </Link>
+            </button>
           ))}
         </div>
         {/* 상단GNB - 로고 */}
@@ -91,24 +155,23 @@ export default function GNB() {
           {/* 로그인 기능이 생기기 전까지 임시 로그인 */}
           <div
             className="px-2 py-1 bg-green-500 rounded text-xs text-white"
-            onClick={handleTestClick}
+            onClick={function () {
+              if (auth) window.localStorage.removeItem("token");
+              else window.localStorage.setItem("token", "Bearer 1234");
+              setAuth(window.localStorage.getItem("token") ? true : false);
+            }}
           >
             TEST
           </div>
         </div>
       </div>
       {/* 하단 GNB(Nav) */}
-      <div
-        className={`h-8 ${
-          navStructure.find((val) => currentUrl.includes(val.mainUrl))
-            .subPadding
-        } border space-x-4`}
-      >
-        {navStructure
-          .find((val) => currentUrl.includes(val.mainUrl))
+      <div className="h-8 px-16 border space-x-4">
+        {nav
+          .find((val) => val.mainNav === selectedmainNav)
           .sub.map((val) => (
             <Link
-              key={val.subNav}
+              key={`subNav-${val.subNav}`}
               className={`text-xs${
                 val.url.includes(currentUrl) ? " text-orange font-semibold" : ""
               }`}
